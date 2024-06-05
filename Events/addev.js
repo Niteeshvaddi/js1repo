f73d22d5257bf960b1f2c16ddf40fcbaf0024c9b
Niteeshvaddi <niteeshvaddi154@gmail.com>
2024-06-05 06:45:00 +0530

    added

---
+++ Events/addev.js
@@ -0,0 +1,11 @@
+function dosum(){
+     let x= document.getElementById(`t1`).value
+     let y= document.getElementById(`t2`).value
+     let sum= eval(x) + eval(y)
+     document.getElementById(`t3`).value = sum
+}
+     function eraseData(){
+        document.getElementById(`t1`).value = ""
+        document.getElementById(`t2`).value = ""
+        document.getElementById(`t3`).value = ""
+     }
